9882a7198cf7f9651993a4b40b32f6442755b128
Sara-Abr <lillestcloud@duck.com>
2025-05-24 18:54:52 +0300

    i will delete this, i just need to clone my project

---
+++ firebase/default-connector/esm/index.esm.js
@@ -0,0 +1,7 @@
+
+export const connectorConfig = {
+  connector: 'default',
+  service: 'firebase-app',
+  location: 'us-central1'
+};
+
---
+++ firebase/default-connector/index.cjs.js
@@ -0,0 +1,7 @@
+
+const connectorConfig = {
+  connector: 'default',
+  service: 'firebase-app',
+  location: 'us-central1'
+};
+exports.connectorConfig = connectorConfig;
